c54682e2d2acd50cde28736b4875f504b1a26ca1
hosalsayed-hub <128558834+hosalsayed-hub@users.noreply.github.com>
2024-08-06 02:53:06 +0300

    number 8

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'render/index'
   devise_for :users
 
-   resources :categories
+  get 'home/index'
+  root to: 'home#index'
+
+  resources :categories
 
   resources :courses do
     get :created, :purchased, :published_unapproved, :pending_review, :latest, :top_rated, :popular, on: :collection
@@ -22,13 +24,11 @@ Rails.application.routes.draw do
   		patch :remove_review
     end
   end
-  get '/health_check', to: proc { [200, {}, ['success']] }
 
   resources :users, only: [:show, :index]
-  
+
   resources :lessons do
     put :sort
   end
-  get "up" => "rails/health#show", as: :rails_health_check
-  root to: 'home#index'
+
 end
