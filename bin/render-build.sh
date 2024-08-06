2f534abd1394bbb5bf82a66267ddb8c26c158cee
hosalsayed-hub <128558834+hosalsayed-hub@users.noreply.github.com>
2024-08-06 05:57:24 +0300

    create 4

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 # exit on error
-set -o errexit
-
-bundle install
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
+bundle exec rails db:drop
+bundle exec rails db:create
 bundle exec rails db:migrate
 bundle exec rails db:seed
