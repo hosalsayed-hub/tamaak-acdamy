557922bf187672f42ccf5735d0b99c4e1cb250ba
hosalsayed-hub <128558834+hosalsayed-hub@users.noreply.github.com>
2024-08-06 06:20:32 +0300

    Update render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # exit on error
-bundle exec rails db:drop
-bundle exec rails db:create
-bundle exec rails db:migrate
-bundle exec rails db:seed
+ rails db:drop
+ rails db:create
+ rails db:migrate
+ rails db:seed
