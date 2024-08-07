4bfc7852fae82ce77a2d8108f42d1827163a004e
hosalsayed-hub <128558834+hosalsayed-hub@users.noreply.github.com>
2024-08-07 04:07:03 +0000

    drop create

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,5 +1,5 @@
 bundle install
-bundle  exec ./bin/rails assets:precompile
-bundle  exec ./bin/rails assets:clean
+bundle  exec ./bin/rails db:drop
+bundle  exec ./bin/rails db:create
 bundle  exec ./bin/rails db:migrate --trace
 
