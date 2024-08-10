dbd2dd08a966274dddac54ae4c30a7a06db78987
hosalsayed-hub <128558834+hosalsayed-hub@users.noreply.github.com>
2024-08-10 07:23:31 +0000

    commit564

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-User.create!(email: 'admin2@tamak.com', password: 'Adm@123', password_confirmation: 'Adm@123')
-User.create!(email: 'hussam2@tamak.com', password: 'Hussam@123', password_confirmation: 'Hussam@123')
-User.create!(email: 'abdullah2@tamak.com', password: 'Abd@123', password_confirmation: 'Aad@123')
+User.create!(email: 'admin@gmail.com', password: 'Admin@123', password_confirmation: 'Admin@123')
+User.create!(email: 'hosalsayed@gmail.com', password: 'Hussam@123', password_confirmation: 'Hussam@123')
+User.create!(email: 'abdulla@gmail.com', password: 'Aa@123', password_confirmation: 'Aa@123')
 
 4.times do
   Category.create!([{
