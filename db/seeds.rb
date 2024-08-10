153f06616550624d41f18bd517efafdc0473657c
hosalsayed-hub <128558834+hosalsayed-hub@users.noreply.github.com>
2024-08-10 06:48:04 +0000

    eeeeee

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,44 @@
 User.create!(email: 'admin@tamak.com', password: 'Adm@123', password_confirmation: 'Adm@123')
 User.create!(email: 'hussam@tamak.com', password: 'Hussam@123', password_confirmation: 'Hussam@123')
 User.create!(email: 'abdullah@tamak.com', password: 'Abd@123', password_confirmation: 'Aad@123')
+
+4.times do
+  Category.create!([{
+    name: Faker::Educator.degree,
+    description: Faker::Lorem.paragraph,
+  }])
+end
+
+10.times do
+  Course.create!([{
+    name: Faker::Educator.course_name,
+    short_description: Faker::Lorem.paragraph,
+    description: Faker::Lorem.paragraph(sentence_count: 2, supplemental: false, random_sentences_to_add: 4),
+    user_id: Faker::Number.between(from: 1, to: 3),
+    category_id: Faker::Number.between(from: 1, to: 3),
+    duration: Faker::Number.between(from: 0, to: 600),
+    price: Faker::Number.between(from: 0, to: 200),
+    published: true,
+    approved: true,
+    language: "English"
+  }])
+end
+
+10.times do
+  Lesson.create!([{
+    name: Faker::Address.unique.city,
+    description: Faker::Lorem.paragraph(sentence_count: 2, supplemental: false, random_sentences_to_add: 4),
+    video_url: "https://www.youtube.com/watch?v=ojXjR33bPOY",
+    course_id: Faker::Number.between(from: 1, to: 9),
+    row_order: Faker::Number.between(from: -50000, to: 50000),
+  }])
+end
+
+1.times do
+  Subscription.create!([{
+    rating: Faker::Number.between(from: 1, to: 5),
+    comment: Faker::Lorem.paragraph(sentence_count: 2, supplemental: false, random_sentences_to_add: 4),
+    course_id: Faker::Number.between(from: 1, to: 20),
+    user_id: Faker::Number.between(from: 1, to: 3),
+  }])
+end
