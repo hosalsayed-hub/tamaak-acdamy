9bd0e4286d0d2b9821028d1daa028e8ebe01b8dc
hosalsayed-hub <128558834+hosalsayed-hub@users.noreply.github.com>
2024-08-10 05:57:22 +0300

    45545454545yuyuyuyuyuvv

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 # exit on error
 bundle install
+
+bundle exec rails assets:precompile
+bundle exec rails assets:clean
+
 bundle exec rails db:migrate --tracee
 bundle exec rails db:seed
